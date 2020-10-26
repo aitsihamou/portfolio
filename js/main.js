db4560f869fd2fa8c5622d767397794c19e1b473
aitsihamou <nourddine.ash@gmail.com>
2020-10-26 23:53:36 +0100

    update global

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
 $('.portfolio ul li').on('click', function () {
     $(this).addClass('active').siblings().removeClass('active');
     if ($(this).data('class') === 'All') {
-      $('.projects .card-group .card').css('display', '');
+      $('.projects .project').css('display', '');
     } else {
-      $('.projects .card-group .card').css('display', 'none');
+      $('.projects .project').css('display', 'none');
       $($(this).data('class')).css('display', '');
     }
   });
