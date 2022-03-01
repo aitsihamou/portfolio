d4a938c9dd2a6b498e2a00af6b78fda8dacb1e54
aitsihamou <nourddine.ash@gmail.com>
2022-03-01 17:07:04 +0100

    update all

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $('.portfolio ul li').on('click', function () {
     $(this).addClass('active').siblings().removeClass('active');
     if ($(this).data('class') === 'All') {
-      $(' .project').parent().css('display', '');
+      $(' .box').css('display', '');
     } else {
-      $('.project').parent().css('display', 'none');
-      $($(this).data('class')).parent().css('display', '');
+      $('.box').css('display', 'none');
+      $($(this).data('class')).css('display', '');
     }
   });
